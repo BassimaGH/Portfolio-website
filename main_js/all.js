265c0e3732181f29b90d0c7343b2b2ef9796069b
bassima_gh <ghba20wu@stident.ju.se>
2022-11-20 19:20:25 +0100

    fixed the dark mode issue

--- main_js/all.js
+++ main_js/all.js
@@ -1,11 +1,40 @@
-var moon_icon = document.getElementById("moon_svg");
+// global variables
+let darkMode = localStorage.getItem("darkMode");
+let moon_icon = document.querySelector("#moon_svg");
 
-moon_icon.onclick = function() {
-    document.body.classList.toggle("dark_mode");
-    if(document.body.classList.contains("dark_mode")) {
-        moon_icon.src = "svg/sun_icon.svg";
-    }
-    else {
-        moon_icon.src = "svg/moon_icon.svg";
-    }
+// adds the darkmode class to the body and localstorage
+function darkModeAdd() {
+    document.body.classList.add("dark_mode");
+    localStorage.setItem("darkMode", "enabled");
 }
+
+// removes the darkmode class to the body and localstorage
+function darkModeRemove() {
+    document.body.classList.remove("dark_mode");
+    localStorage.setItem("darkMode", null);
+}
+
+// loads all the functions
+function loadHandler(){
+    moon_icon = document.querySelector("#moon_svg");
+
+    // makes the state of the page (dark/light) stay even after refreshing
+    if(darkMode === "enabled") {
+        darkModeAdd();
+    }
+
+    moon_icon.addEventListener("click", () => {
+        // to update the darkmode variable in the localstorage
+        darkMode = localStorage.getItem("darkMode");
+
+        // if statement that executes the darkmode function
+        if(darkMode !== "enabled") {
+            darkModeAdd();
+        } else {
+            darkModeRemove();
+        }
+    });
+
+}
+
+window.addEventListener("load", loadHandler);
